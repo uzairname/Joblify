2f8ea0881694f6943d01a2ff2d092a4b67bb4ffa
Uzair <uzair.m6d@gmail.com>
2023-01-29 02:11:24 -0500

    add job postings endpoint

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -1,5 +1,58 @@
 import mongoengine as me
+import time
+import os
+import pymongo as pm
 
+
+def connect_db(db_name):
+
+    start_time = time.time()
+    uri = os.environ.get('MONGODB_URI')
+    if uri is None:
+        raise RuntimeError("MONGODB_URI not found in environment.")
+
+    me.connect(host=uri, db=db_name)
+    client = pm.MongoClient(os.environ.get('MONGODB_URI'))
+    db = client[db_name]
+
+    print(f"Connected to database \"{db_name}\" in {time.time() - start_time} seconds.")
+    return db
+
+class Database:
+    def __init__(self, db_name):
+        self.db = connect_db(db_name)
+
+    def run_migrations(self):
+        """
+        This is run every time the bot starts.
+        """
+
+        # find the database version
+        try:
+            version = DbSettings.objects.get().database_version
+        except me.DoesNotExist:
+            setting = DbSettings(database_version=1)
+            setting.save()
+            version = 0
+
+        # run migrations
+        if version == 0:
+            self.v1()
+            version = 1
+
+        if version == 1:
+            self.v2()
+            version = 2
+
+        print("Ran migrations.")
+
+    def v1(self):
+        DbSettings.objects.get().update(database_version=1)
+
+    def v2(self):
+        self.db["users"].update_many({}, {"$set": {"name":None}})
+        self.db["resumes"].update_many({}, {"$set": {"user":None}})
+        DbSettings.objects.get().update(database_version=2)
 
 
 class User(me.Document):
@@ -16,12 +69,12 @@ class Resume(me.Document):
     meta = {'collection': 'resumes'}
 
 
-class JobListing(me.Document):
+class JobPosting(me.Document):
 
     description = me.StringField(required=True)
     user = me.ReferenceField(User, reverse_delete_rule=me.CASCADE)
 
-    meta = {'collection': 'job_listings'}
+    meta = {'collection': 'job_postings'}
 
 
 
@@ -38,9 +91,3 @@ class DbSettings(me.Document):
     }
 
 
-
-__all__ = [
-    'DbSettings',
-    'User',
-    'Resume',
-]
--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,9 @@
 import jinja2
 from flask import Flask, render_template, request
-import pymongo as pm
 import os
-import time
-import mongoengine as me
 
-from flask_cors import CORS, cross_origin
-
-from database.models import *
+from database.models import Database
+import database.models as models
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -42,11 +38,40 @@ def upload_file():
     data = request.form
     print("resume data", data)
 
-    resume = Resume(content=data, user=user)
+    resume = models.Resume(content=data, user=user)
     resume.save()
 
     print(data)
     return 1
+
+
+
+@app.route('/api/job-posting', methods=['POST'])
+# @cross_origin(supports_credentials=True)
+def upload_file():
+    print("uploaded")
+
+    data = request.form
+    print("job data", data)
+
+    job_posting = models.JobPosting(description=data, user=user)
+    job_posting.save()
+
+    print(data)
+    return 1
+
+
+@app.route('/api/matches', methods=['GET'])
+# @cross_origin(supports_credentials=True)
+def get_matches():
+    print("getting matches")
+
+    job_posting = models.JobPosting.objects.get(id=job_posting_id)
+    job_posting = models.JobPosting.objects.get(id=job_posting_id)
+
+    matches = models.Resume.objects(user=job_posting.user)
+    return matches
+
 
 
 # @app.route('/api/resume', methods=['GET'])
@@ -57,69 +82,16 @@ def upload_file():
 #     return 1
 
 
-
-
-def connect_db(db_name):
-
-    start_time = time.time()
-    uri = os.environ.get('MONGODB_URI')
-    if uri is None:
-        raise RuntimeError("MONGODB_URI not found in environment.")
-
-    me.connect(host=uri, db=db_name)
-    client = pm.MongoClient(os.environ.get('MONGODB_URI'))
-    db = client[db_name]
-
-    print(f"Connected to database \"{db_name}\" in {time.time() - start_time} seconds.")
-    return db
-
-class Database:
-    def __init__(self, db_name):
-        self.db = connect_db(db_name)
-
-    def run_migrations(self):
-        """
-        This is run every time the bot starts.
-        """
-
-        # find the database version
-        try:
-            version = DbSettings.objects.get().database_version
-        except me.DoesNotExist:
-            setting = DbSettings(database_version=1)
-            setting.save()
-            version = 0
-
-        # run migrations
-        if version == 0:
-            self.v1()
-            version = 1
-
-        if version == 1:
-            self.v2()
-            version = 2
-
-        print("Ran migrations.")
-
-    def v1(self):
-        DbSettings.objects.get().update(database_version=1)
-
-    def v2(self):
-        self.db["users"].update_many({}, {"$set": {"name":None}})
-        self.db["resumes"].update_many({}, {"$set": {"user":None}})
-        DbSettings.objects.get().update(database_version=2)
-
-
 if __name__ == '__main__':
     db = Database("development")
     db.run_migrations()
 
-    Resume.drop_collection()
-    User.drop_collection()
+    models.Resume.drop_collection()
+    models.User.drop_collection()
 
-    user = User(name="guy")
+    user = models.User(name="guy")
     user.save()
-    resume = Resume(content="resume", user=user)
+    resume = models.Resume(content="resume", user=user)
     resume.save()
 
     app.run(debug=True, port=5001)
