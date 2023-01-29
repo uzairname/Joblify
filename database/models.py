f6665814a94860abaad45435a0fcc5c754c6f047
Uzair <uzair.m6d@gmail.com>
2023-01-28 22:18:46 -0500

    mongodb connection

---
+++ database/models.py
@@ -0,0 +1,40 @@
+import mongoengine as me
+
+
+
+
+class User(me.Document):
+
+    name = me.StringField(required=True)
+
+    meta = {'collection': 'users'}
+
+
+class Resume(me.Document):
+
+    content = me.StringField()
+    user = me.ReferenceField(User, reverse_delete_rule=me.CASCADE)
+
+    meta = {'collection': 'resumes'}
+
+
+
+
+
+
+
+class DbSettings(me.Document):
+    database_version = me.IntField()
+
+    meta = {
+        'collection': 'db_settings',
+        'max_documents': 1,
+    }
+
+
+
+__all__ = [
+    'DbSettings',
+    'User',
+    'Resume',
+]
