91c15898ff7b1348aa2b47cc77070a0751b8697c
Uzair <uzair.m6d@gmail.com>
2023-01-28 19:40:22 -0500

    results page

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,62 @@
 
-import cohere
-co = cohere.Client('uY0ZdCM1A165gyUg8RNIqc1kv7N5uj1uwAWOh7Cb')
 
-response = co.embed(
-  texts=[
-    "abc"
-  ]
-)
+from spacy.pipeline import EntityRuler
+from spacy.language import Language
+from spacy.lang.en import English
+import numpy as np
 
-print(response)
+import srsly
+import json
+
+from flask import Flask, render_template
+
+patterns = srsly.read_jsonl("skill_patterns.jsonl")
+
+
+nlp = English()
+
+ruler = nlp.add_pipe("entity_ruler")
+ruler.add_patterns(patterns)
+
+
+def extract_keywords(filename):
+    with open(filename, 'r') as file:
+        txt = file.read().rstrip()
+
+    doc = nlp(txt)
+    keywords = list(doc.ents)
+    # remove duplicates
+    keywords = [str(k) for k in keywords]
+    keywords = list(set(keywords))
+
+    return keywords
+
+
+def calculate_score(resume_kws, job_kws):
+
+    resume_kws = np.array(resume_kws)
+    job_kws = np.array(job_kws)
+
+    match_count = 0
+    for i in resume_kws:
+      for j in job_kws:
+        if i == j:
+          match_count += 1
+          continue
+
+    score = 2 * match_count / (resume_kws.size + job_kws.size)
+    return score
+
+
+def calculate_similarity(resume_path, job_path):
+    resume_keywords = extract_keywords(resume_path)
+    job_keywords = extract_keywords(job_path)
+
+    score = calculate_score(resume_keywords, job_keywords)
+    return score
+
+
+Flask.render_template('results.html')
+
+
 
