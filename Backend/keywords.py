66d0a37e08697f9317c1f992aed686bfc6fe0174
Uzair <uzair.m6d@gmail.com>
2023-01-29 00:18:08 -0500

    copy files to templates

---
+++ Backend/keywords.py
@@ -0,0 +1,60 @@
+from spacy.pipeline import EntityRuler
+from spacy.language import Language
+from spacy.lang.en import English
+import numpy as np
+
+import srsly
+import json
+
+from flask import Flask, render_template
+
+patterns = srsly.read_jsonl("skill_patterns.jsonl")
+
+
+nlp = English()
+
+ruler = nlp.add_pipe("entity_ruler")
+ruler.add_patterns(patterns)
+
+
+def extract_keywords(filename):
+    with open(filename, 'r') as file:
+        txt = file.read().rstrip()
+
+    doc = nlp(txt)
+    keywords = list(doc.ents)
+    # remove duplicates
+    keywords = [str(k) for k in keywords]
+    keywords = list(set(keywords))
+
+    return keywords
+
+
+def calculate_score(resume_kws, job_kws):
+
+    resume_kws = np.array(resume_kws)
+    job_kws = np.array(job_kws)
+
+    match_count = 0
+    for i in resume_kws:
+      for j in job_kws:
+        if i == j:
+          match_count += 1
+          continue
+
+    score = 2 * match_count / (resume_kws.size + job_kws.size)
+    return score
+
+
+def calculate_similarity(resume_path, job_path):
+    resume_keywords = extract_keywords(resume_path)
+    job_keywords = extract_keywords(job_path)
+
+    score = calculate_score(resume_keywords, job_keywords)
+    return score
+
+
+Flask.render_template('results.html')
+
+
+
--- Backend/main.py
+++ Backend/main.py
@@ -1,62 +1,111 @@
+import jinja2
+from flask import Flask, render_template, request
+import pymongo as pm
+import os
+import time
+import mongoengine as me
+
+from flask_cors import CORS, cross_origin
+
+from database.models import *
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
-from spacy.pipeline import EntityRuler
-from spacy.language import Language
-from spacy.lang.en import English
-import numpy as np
+app = Flask(__name__)
 
-import srsly
-import json
-
-from flask import Flask, render_template
-
-patterns = srsly.read_jsonl("skill_patterns.jsonl")
+@app.route('/')
+def index():
+    return render_template(jinja_env.get_template("upload.html"))
 
 
-nlp = English()
+@app.route('/api/resume', methods=['POST'])
+# @cross_origin(supports_credentials=True)
+def upload_file():
+    print("uploaded")
 
-ruler = nlp.add_pipe("entity_ruler")
-ruler.add_patterns(patterns)
+    data = request.form
+    print(data)
 
+    resume = Resume(content=data, user=user)
+    resume.save()
 
-def extract_keywords(filename):
-    with open(filename, 'r') as file:
-        txt = file.read().rstrip()
-
-    doc = nlp(txt)
-    keywords = list(doc.ents)
-    # remove duplicates
-    keywords = [str(k) for k in keywords]
-    keywords = list(set(keywords))
-
-    return keywords
+    print(data)
+    return 1
 
 
-def calculate_score(resume_kws, job_kws):
-
-    resume_kws = np.array(resume_kws)
-    job_kws = np.array(job_kws)
-
-    match_count = 0
-    for i in resume_kws:
-      for j in job_kws:
-        if i == j:
-          match_count += 1
-          continue
-
-    score = 2 * match_count / (resume_kws.size + job_kws.size)
-    return score
-
-
-def calculate_similarity(resume_path, job_path):
-    resume_keywords = extract_keywords(resume_path)
-    job_keywords = extract_keywords(job_path)
-
-    score = calculate_score(resume_keywords, job_keywords)
-    return score
-
-
-Flask.render_template('results.html')
+# @app.route('/api/resume', methods=['GET'])
+# # @cross_origin(supports_credentials=True)
+# def get_test():
+#     print("get request")
+#
+#     return 1
 
 
 
+
+def connect_db(db_name):
+
+    start_time = time.time()
+    uri = os.environ.get('MONGODB_URI')
+    if uri is None:
+        raise RuntimeError("MONGODB_URI not found in environment.")
+
+    me.connect(host=uri, db=db_name)
+    client = pm.MongoClient(os.environ.get('MONGODB_URI'))
+    db = client[db_name]
+
+    print(f"Connected to database \"{db_name}\" in {time.time() - start_time} seconds.")
+    return db
+
+class Database:
+    def __init__(self, db_name):
+        self.db = connect_db(db_name)
+
+    def run_migrations(self):
+        """
+        This is run every time the bot starts.
+        """
+
+        # find the database version
+        try:
+            version = DbSettings.objects.get().database_version
+        except me.DoesNotExist:
+            setting = DbSettings(database_version=1)
+            setting.save()
+            version = 0
+
+        # run migrations
+        if version == 0:
+            self.v1()
+            version = 1
+
+        if version == 1:
+            self.v2()
+            version = 2
+
+        print("Ran migrations.")
+
+    def v1(self):
+        DbSettings.objects.get().update(database_version=1)
+
+    def v2(self):
+        self.db["users"].update_many({}, {"$set": {"name":None}})
+        self.db["resumes"].update_many({}, {"$set": {"user":None}})
+        DbSettings.objects.get().update(database_version=2)
+
+
+if __name__ == '__main__':
+    db = Database("development")
+    db.run_migrations()
+
+    Resume.drop_collection()
+    User.drop_collection()
+
+    user = User(name="guy")
+    user.save()
+    resume = Resume(content="resume", user=user)
+    resume.save()
+
+    app.run(debug=True, port=5001)
